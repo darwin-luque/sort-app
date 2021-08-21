cd6a8975e7d38af9d7adf71bdcbef56a3106357d
Darwin Luque <darwin.luque.98@gmail.com>
2021-08-20 22:24:06 -0600

    add Sortable interface

--- build/index.js
+++ build/index.js
@@ -5,4 +5,4 @@ var NumbersCollection_1 = require("./NumbersCollection");
 var collection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(collection);
 sorter.sort();
-console.log(sorter.collection.data);
+console.log(collection.data);
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { NumbersCollection } from './NumbersCollection';
 const collection = new NumbersCollection([10, 3, -5, 0])
 const sorter = new Sorter(collection);
 sorter.sort();
-console.log(sorter.collection.data);
+console.log(collection.data);
